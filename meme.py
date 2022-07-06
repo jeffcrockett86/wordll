b17a607605f596aad8b356ce31fdc15f18761be2
Jeff <jeffcrockett03@gmail.com>
2022-07-06 17:06:31 -0400

    can now print rows and cell names individually

--- meme.py
+++ meme.py
@@ -2,30 +2,34 @@ class Table:
     def __init__(self, name):
         self.name = name
         self.rows = []
+        self.html = '<table> </table>'
 
 class Row:
     def __init__(self, name, parent):
         self.parent = parent
         self.name = name
         self.cells = []
+        self.html = '<tr> </tr>'
 
 class Col:
     def __init__(self, name, parent):
         self.parent = parent
         self.name = name
         self.cells = []
+        self.html = ''
 
 class Cell:
     def __init__(self, name, parent):
         self.parent = parent
         self.name = name
         self.tables = []
+        self.html = '<td> </td>'
 
 def make_table():
     t = Table('Wordle')
     t.rows = [Row(f'Guess {i}', t) for i in range(6)]
     for row in t.rows:
-        row.cells = [Cell(f'Letter {i}', parent=row) for i in range(5)]
+        row.cells = [Cell(f'Letter {i}', parent=row).html for i in range(5)]
     return t
 # class M:
 #     def __init__(self, name):
@@ -70,3 +74,6 @@ def make_table():
 #         self.parent = parent
 #         self.name = ''
 #         self.A = []
+m = make_table()
+print([row.name for row in m.rows])
+print([cell for row in m.rows for cell in row.cells])
