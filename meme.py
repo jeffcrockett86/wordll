9252be282c39ffaea5368d9a615fbbadc0944a0c
Jeff <jeffcrockett03@gmail.com>
2022-07-06 16:43:41 -0400

    added make_table

--- meme.py
+++ meme.py
@@ -1,26 +1,32 @@
-# class Table:
-#     def __init__(self, name):
-#         self.name = name
-#         self.rows = []
-# class Row:
-#     def __init__(self, parent):
-#         self.parent = parent
-#         self.name = ''
-#         self.cells = []
+class Table:
+    def __init__(self, name):
+        self.name = name
+        self.rows = []
 
-# class Col:
-#     def __init__(self, parent):
-#         self.parent = parent
-#         self.name = ''
-#         self.cells = []
+class Row:
+    def __init__(self, name, parent):
+        self.parent = parent
+        self.name = name
+        self.cells = []
 
-# class Cell:
-#     def __init__(self, parent):
-#         self.parent = parent
-#         self.name = ''
-#          self.tables = []
+class Col:
+    def __init__(self, name, parent):
+        self.parent = parent
+        self.name = name
+        self.cells = []
 
+class Cell:
+    def __init__(self, name, parent):
+        self.parent = parent
+        self.name = name
+        self.tables = []
 
+def make_table():
+    t = Table('Wordle')
+    t.rows = [Row(f'Guess {i}', t) for i in range(6)]
+    for row in t.rows:
+        row.cells = [Cell(f'Letter {i}', parent=row) for i in range(5)]
+    return t
 # class M:
 #     def __init__(self, name):
 #         self.name = name
@@ -43,24 +49,24 @@
 #         self.name = ''
 #         self.M = []
 
-class A:
-    def __init__(self, name):
-        self.name = name
-        self.C = []
-class C:
-    def __init__(self, parent):
-        self.parent = parent
-        self.name = ''
-        self.G = []
+# class A:
+#     def __init__(self, name):
+#         self.name = name
+#         self.C = []
+# class C:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.G = []
 
-class T:
-    def __init__(self, parent):
-        self.parent = parent
-        self.name = ''
-        self.G = []
+# class T:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.G = []
 
-class G:
-    def __init__(self, parent):
-        self.parent = parent
-        self.name = ''
-        self.A = []
+# class G:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.A = []
