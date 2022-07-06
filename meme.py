861ff74f125a6408d681f47249b6a10dadc5d6d0
Jeff <jeffcrockett03@gmail.com>
2022-07-06 15:07:12 -0400

    added DNA

--- meme.py
+++ meme.py
@@ -1,21 +1,66 @@
-class Table:
+# class Table:
+#     def __init__(self, name):
+#         self.name = name
+#         self.rows = []
+# class Row:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.cells = []
+
+# class Col:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.cells = []
+
+# class Cell:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#          self.tables = []
+
+
+# class M:
+#     def __init__(self, name):
+#         self.name = name
+#         self._E = []
+# class _E:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.E = []
+
+# class _M:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.E = []
+
+# class E:
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.name = ''
+#         self.M = []
+
+class A:
     def __init__(self, name):
         self.name = name
-        self.rows = []
-class Row:
+        self.C = []
+class C:
     def __init__(self, parent):
         self.parent = parent
         self.name = ''
-        self.cells = []
+        self.G = []
 
-class Col:
+class T:
     def __init__(self, parent):
         self.parent = parent
         self.name = ''
-        self.cells = []
+        self.G = []
 
-class Cell:
+class E:
     def __init__(self, parent):
         self.parent = parent
         self.name = ''
-        self.tables = []
+        self.A = []
